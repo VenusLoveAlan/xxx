c1f2facaae50c4c13a8bcd1176add7cebbcf47b3
VenusLoveAlan <whanghao243249386@163.com>
2019-06-02 11:06:34 +0800

    xx

---
+++ scripts/test_allure_demo.py
@@ -0,0 +1,19 @@
+
+
+class TestAllureDemo:
+
+    def test_login1(self):
+        print('test_login1')
+        assert True
+
+    def test_login2(self):
+        print('test_login2')
+        assert True
+
+    def test_login3(self):
+        print('test_login3')
+        assert True
+    
+    def test_login4(self):
+        print('test_login4')
+        assert True
